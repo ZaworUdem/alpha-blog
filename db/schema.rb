99263112651f74e92a13dadae660f75f6ed144e9
Piotr Zaworski <fuhorte@gmail.com>
2019-07-12 12:56:46 +0200

    Create users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_141100) do
+ActiveRecord::Schema.define(version: 2019_07_12_105118) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2019_06_19_141100) do
   create_table "feedbacks", force: :cascade do |t|
     t.string "author"
     t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
