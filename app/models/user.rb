dbfd21c86d6e8f9acc5c04babfaf1e798574cf87
Piotr Zaworski <fuhorte@gmail.com>
2019-07-13 22:32:38 +0200

    Signup functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
-class User < ActiveRecod::Base
+class User < ActiveRecord::Base
 
     has_many :articles
 
     before_save {self.email = email.downcase}
 
-    validate :username, presence: true, uniqueness: { case_sensitive: false } ,lenght: {minimum: 3, maximum: 25}    
+    validates :username, presence: true, uniqueness: { case_sensitive: false } ,length: {minimum: 3, maximum: 25}    
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validate :email, presence: true, uniqueness: { case_sensitive: false }, lenght: {maximum: 105}, format: { width: VALID_EMAIL_REGEX }
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 105}, format: { with: VALID_EMAIL_REGEX }
 
     has_secure_password
 
