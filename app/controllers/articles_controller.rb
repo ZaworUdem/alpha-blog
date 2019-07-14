6ecde01f4f8e1ad13a6fa9812851c5d0e403c32e
Piotr Zaworski <fuhorte@gmail.com>
2019-07-14 12:08:27 +0200

    Update current user to be creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,7 +32,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
        # @article.save
        # redirect_to article_path(@aricle)
 
